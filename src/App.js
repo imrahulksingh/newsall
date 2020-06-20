d09ef211f9f62e58be2eac2314666134510ddef0
Rahul Singh <rahulsingh@Rahuls-MacBook-Air.local>
2020-06-20 13:21:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Route path="/about" component={About} />
         <Route path="/technology" component={Technology} />
         <Route path="/opinion" exact component={Opinion} />
-        <Route path="/home" exact component={Homefetchxml} />
+        <Route path="/newsall/home" exact component={Homefetchxml} />
         <Route path="/top-stories" exact component={Topstoriesfetchxml} />
       </Router>
     </div>
