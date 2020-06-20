88aecf4e680ed30c448df07913df8cd8a07ad1ac
Rahul Singh <rahulsingh@Rahuls-MacBook-Air.local>
2020-06-20 17:29:31 +0530

    third commit

---
+++ build/precache-manifest.58b518f01ea251ee24637bec4f1e2121.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b15772a5b1f407ff7168919bc2172fb",
+    "url": "/mynews/index.html"
+  },
+  {
+    "revision": "48b2a404b02333d59a7e",
+    "url": "/mynews/static/css/main.9bd1156e.chunk.css"
+  },
+  {
+    "revision": "465a5019465a4eedb7d1",
+    "url": "/mynews/static/js/2.7ff0fc70.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mynews/static/js/2.7ff0fc70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09c8d63ffb07e5e3dfe",
+    "url": "/mynews/static/js/3.09694087.chunk.js"
+  },
+  {
+    "revision": "de97f04472eb635c339a",
+    "url": "/mynews/static/js/4.df6344d8.chunk.js"
+  },
+  {
+    "revision": "48b2a404b02333d59a7e",
+    "url": "/mynews/static/js/main.495415f3.chunk.js"
+  },
+  {
+    "revision": "8a85d11317d4317e836c",
+    "url": "/mynews/static/js/runtime-main.ab292514.js"
+  },
+  {
+    "revision": "5bed0f65feac5fe1b0459d43e8cff998",
+    "url": "/mynews/static/media/bs.5bed0f65.jpg"
+  },
+  {
+    "revision": "27d9aa9edf16826729a59ef9cfcce6f6",
+    "url": "/mynews/static/media/dna.27d9aa9e.png"
+  },
+  {
+    "revision": "0e8148241ba203547f1a46faae79613a",
+    "url": "/mynews/static/media/et.0e814824.jpg"
+  },
+  {
+    "revision": "fc75d28f690713bc717b2aaffa76fb8d",
+    "url": "/mynews/static/media/et.fc75d28f.cms"
+  },
+  {
+    "revision": "fb0f5e3ccb3ab67f25b02f8a83e9e60e",
+    "url": "/mynews/static/media/ht.fb0f5e3c.jpg"
+  },
+  {
+    "revision": "cc89ebdd2e12b6be03da84f9a0bdbb7e",
+    "url": "/mynews/static/media/it.cc89ebdd.png"
+  },
+  {
+    "revision": "ae37ff392f48db0b31644b1570a0dc37",
+    "url": "/mynews/static/media/ol.ae37ff39.png"
+  },
+  {
+    "revision": "c35a23f875e3611e81c0d93f0beeeda1",
+    "url": "/mynews/static/media/ol.c35a23f8.jpg"
+  },
+  {
+    "revision": "95234c4742a1ed6662db8b8730f1b83c",
+    "url": "/mynews/static/media/th.95234c47.png"
+  },
+  {
+    "revision": "a23b91f250e28d33d1c45c0af2134b0c",
+    "url": "/mynews/static/media/thehindu-logo.a23b91f2.png"
+  },
+  {
+    "revision": "7bc9888ce647fa0847a4d6275eb62ede",
+    "url": "/mynews/static/media/zeenews.7bc9888c.jpg"
+  }
+]);
